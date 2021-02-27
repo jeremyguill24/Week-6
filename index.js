dd9e9dfd1b049de8256bb86ae1e020d9e127edde
Jeremy Guill <jeremy_guill@hotmail.com>
2021-02-27 13:04:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+class Card {
+    constructor(suit, rank, value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+}
+
+class Deck {
+    constructor(cards) {
+        this.cards = cards;
+    }
+
+    createDeck() {
+        let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
+        let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+        let values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < ranks.length; j++) {
+                this.cards.push(new Card(suits[i], ranks[j], values[j]));
+            }
+        }
+
+    }
+
+
+    shuffleDeck() {
+        let location1, location2, tmp;
+        for (let i = 0; i < 1000; i++) {
+            location1 = Math.floor((Math.random() * this.cards.length));
+            location2 = Math.floor((Math.random() * this.cards.length));
+            tmp = this.cards[location1];
+            this.cards[location1] = this.cards[location2];
+            this.cards[location2] = tmp;
+        }
+    }
+}
+class Player {
+    constructor(name) {
+        this.playerName = name;
+        this.playerCards = [];
+    }
+}
+class Board {
+    constructor() {
+        this.cardsInMiddle = [];
+        this.players = [];
+    }
+    start(playerOneName, playerTwoName) {
+        this.players.push(new Player(playerOneName));
+        this.players.push(new Player(playerTwoName));
+        let d = new Deck();
+        d.createDeck();
+        d.shuffleDeck();
+        this.players[0].playerCards = d.cards.slice(0, 26);
+        this.players[1].playerCards = d.cards.slice(26, 52);
+    }
+}
+
+var newGame = new Board();
+newGame.start('Jeremy', 'Mike');
+
+
